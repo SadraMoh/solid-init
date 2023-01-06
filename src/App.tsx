acf94c0b1f2bfdcee45ed0cb05ba740f63251537
sadramoh <shockylemi@gmail.com>
2023-01-06 21:40:04 +0330

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import type { Component } from 'solid-js';
+import Layout from './shared/layout/Layout';
+
+const App: Component = () => {
+  return (
+    <Layout>
+      <h2>Hello World!</h2>
+    </Layout>
+  );
+};
+
+export default App;
---
+++ src/shared/layout/Layout.tsx
@@ -0,0 +1,37 @@
+import { Component, JSX } from "solid-js";
+import styles from "../layout/layout.module.scss";
+
+type Props = {
+  children?: JSX.Element
+  header?: JSX.Element | string
+};
+
+const Layout: Component<Props> = ({ header, children }) => {
+  return (
+    <div class={styles.layout}>
+      <header>
+        {header ?? <h1>Recepies</h1>}
+      </header>
+      <main>
+        {children}
+      </main>
+      <footer>
+        <nav>
+          <ul>
+            <li>
+              list
+            </li>
+            <li>
+              new
+            </li>
+            <li>
+              favourites
+            </li>
+          </ul>
+        </nav>
+      </footer>
+    </div>
+  );
+}
+
+export default Layout;
